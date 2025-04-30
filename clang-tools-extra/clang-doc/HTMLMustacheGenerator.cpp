290ea15206b4b3589fbc745def8b25dd15430d63
Paul Kirth <paulkirth@google.com>
2025-04-30 08:10:20 -0700

    [clang-doc] Add helpers for Template config
    This patch adds or fills in some helper functions related to template
    setup when initializing the mustache backend. It was split from #133161.
    
    Co-authored-by: Peter Chou <peter.chou@mail.utoronto.ca>

--- clang-tools-extra/clang-doc/HTMLMustacheGenerator.cpp
+++ clang-tools-extra/clang-doc/HTMLMustacheGenerator.cpp
@@ -47,7 +47,7 @@ public:
     ErrorOr<std::unique_ptr<MemoryBuffer>> BufferOrError =
         MemoryBuffer::getFile(FileName);
     if (auto EC = BufferOrError.getError())
-      return createFileError("cannot open file", EC);
+      return createFileError("cannot open file: " + FileName, EC);
     std::unique_ptr<MemoryBuffer> Buffer = std::move(BufferOrError.get());
     StringRef FileContent = Buffer->getBuffer();
     registerPartial(Name.str(), FileContent.str());
@@ -61,7 +61,41 @@ static std::unique_ptr<MustacheTemplateFile> NamespaceTemplate = nullptr;
 
 static std::unique_ptr<MustacheTemplateFile> RecordTemplate = nullptr;
 
+static Error
+setupTemplate(std::unique_ptr<MustacheTemplateFile> &Template,
+              StringRef TemplatePath,
+              std::vector<std::pair<StringRef, StringRef>> Partials) {
+  auto T = MustacheTemplateFile::createMustacheFile(TemplatePath);
+  if (auto EC = T.getError())
+    return createFileError("cannot open file: " + TemplatePath, EC);
+  Template = std::move(T.get());
+  for (const auto [Name, FileName] : Partials) {
+    if (auto Err = Template->registerPartialFile(Name, FileName))
+      return Err;
+  }
+  return Error::success();
+}
+
 static Error setupTemplateFiles(const clang::doc::ClangDocContext &CDCtx) {
+  std::string NamespaceFilePath =
+      CDCtx.MustacheTemplates.lookup("namespace-template");
+  std::string ClassFilePath = CDCtx.MustacheTemplates.lookup("class-template");
+  std::string CommentFilePath =
+      CDCtx.MustacheTemplates.lookup("comments-template");
+  std::string FunctionFilePath =
+      CDCtx.MustacheTemplates.lookup("function-template");
+  std::string EnumFilePath = CDCtx.MustacheTemplates.lookup("enum-template");
+  std::vector<std::pair<StringRef, StringRef>> Partials = {
+      {"Comments", CommentFilePath},
+      {"FunctionPartial", FunctionFilePath},
+      {"EnumPartial", EnumFilePath}};
+
+  if (Error Err = setupTemplate(NamespaceTemplate, NamespaceFilePath, Partials))
+    return Err;
+
+  if (Error Err = setupTemplate(RecordTemplate, ClassFilePath, Partials))
+    return Err;
+
   return Error::success();
 }
 
--- clang-tools-extra/unittests/clang-doc/HTMLMustacheGeneratorTest.cpp
+++ clang-tools-extra/unittests/clang-doc/HTMLMustacheGeneratorTest.cpp
@@ -9,6 +9,7 @@
 #include "ClangDocTest.h"
 #include "Generators.h"
 #include "Representation.h"
+#include "support/Utils.h"
 #include "clang/Basic/Version.h"
 #include "llvm/Support/Path.h"
 #include "llvm/Testing/Support/Error.h"
@@ -86,8 +87,16 @@ TEST(HTMLMustacheGeneratorTest, generateDocs) {
   assert(G && "Could not find HTMLMustacheGenerator");
   ClangDocContext CDCtx = getClangDocContext();
 
-  StringRef RootDir = "";
-  EXPECT_THAT_ERROR(G->generateDocs(RootDir, {}, CDCtx), Succeeded())
+  unittest::TempDir RootTestDirectory("generateDocsTest", /*Unique=*/true);
+  CDCtx.OutDirectory = RootTestDirectory.path();
+
+  // This seems wrong, but its unclear how else we would test this...
+  SmallString<128> AssetPath("../../../../../share/clang-doc");
+  llvm::sys::path::native(AssetPath);
+  getMustacheHtmlFiles(AssetPath, CDCtx);
+
+  EXPECT_THAT_ERROR(G->generateDocs(RootTestDirectory.path(), {}, CDCtx),
+                    Succeeded())
       << "Failed to generate docs.";
 }
 
